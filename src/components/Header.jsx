512fcf02de2fdad92804f81607d65ddfdb443238
Alexander Radaev <alxdr27@gmail.com>
2020-08-11 18:48:50 +0200

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className = 'header'>
+    <img src='https://www.meritnation.com/blog/wp-content/upLoads/5_soccerball-transparent-png-29.png' alt='logotipo'></img>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className = 'content'>
+    <div>
+      <img src='https://www.viajejet.com/wp-content/viajes/paisaje-marino-lago-jackson.jpg' alt='paisaje'></img>
+    </div>
+    <div>
+      ava+description
+    </div>
+    <div>
+      my posts
+      <div>
+        new post
+      </div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  </div>
+
+}
+
+export default Profile; 
